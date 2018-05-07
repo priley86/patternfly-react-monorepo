9490bad39d5233d2a2cc568f68bf300226fb7ef2
Patrick Riley <priley86@gmail.com>
2018-05-07 10:51:33 -0400

    remove webpack img and font overrides

--- storybook/webpack.config.js
+++ storybook/webpack.config.js
@@ -17,7 +17,7 @@ const packages = readdirSync(path.resolve(__dirname, PCKGS))
   );
 
 module.exports = (baseConfig, env, defaultConfig) => {
-  // add the root path for so root references can be used in stories
+  // add the root path so root references can be used in stories
   defaultConfig.resolve.modules.push(path.resolve(__dirname, '../'));
 
   defaultConfig.module.rules.push(
@@ -54,49 +54,6 @@ module.exports = (baseConfig, env, defaultConfig) => {
           }
         }
       ]
-    },
-    // Images
-    {
-      test: /\.(png|jpg|gif)(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'url-loader',
-      query: {
-        limit: 8192, // inline base64 URLs for <=8k images, direct URLs for the rest
-        name: '[name].[ext]'
-      }
-    },
-
-    // Fonts and svg images
-    {
-      test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'url-loader',
-      query: {
-        limit: 8192,
-        mimetype: 'application/font-woff',
-        name: '[name].[ext]'
-      }
-    },
-    {
-      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'url-loader',
-      query: {
-        limit: 8192,
-        mimetype: 'application/octet-stream',
-        name: '[name].[ext]'
-      }
-    },
-    {
-      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'file-loader',
-      query: { name: '[name].[ext]' }
-    },
-    {
-      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'url-loader',
-      query: {
-        limit: 8192,
-        mimetype: 'image/svg+xml',
-        name: '[name].[ext]'
-      }
     }
   );
 
